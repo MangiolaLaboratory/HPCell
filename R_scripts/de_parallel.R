d12d68eb8d39a37fccbec1100e398ac424d8e2db
Stefano Mangiola <mangiolastefano@gmail.com>
2023-08-23 17:05:51 +1000

    made it work outside function

--- R_scripts/de_parallel.R
+++ R_scripts/de_parallel.R
@@ -1,46 +1,48 @@
 library(tidyverse)
 library(targets)
 library(glue)
-library(CuratedAtlasQueryR)
+
 
 # Get input
 
-my_data = tidySummarizedExperiment::se
-tibble(se = list(my_data)) |> saveRDS(glue("temp_data.rds"))
+my_data = 
+  tidySummarizedExperiment::se |> 
+  tidybulk::keep_abundant()
+tibble(name = "my_data", se = list(my_data)) |> saveRDS(glue("temp_data.rds"))
 my_store = tempfile(tmpdir = ".")
 
-library(future)
-library("future.batchtools")
-
-small_slurm = 
-  tar_resources(
-    future = tar_resources_future(
-      plan = tweak(
-        batchtools_slurm,
-        template = glue("/stornext/Bioinf/data/bioinf-data/Papenfuss_lab_projects/people/mangiola.s/third_party_sofware/slurm_batchtools.tmpl"),
-        resources = list(
-          ncpus = 2,
-          memory = 40000,
-          walltime = 172800
-        )
-      )
-    )
-  )
-
-big_slurm = 
-  tar_resources(
-    future = tar_resources_future(
-      plan = tweak(
-        batchtools_slurm,
-        template = glue("/stornext/Bioinf/data/bioinf-data/Papenfuss_lab_projects/people/mangiola.s/third_party_sofware/slurm_batchtools.tmpl"),
-        resources = list(
-          ncpus = 19,
-          memory = 6000,
-          walltime = 172800
-        )
-      )
-    )
-  )
+# library(future)
+# library("future.batchtools")
+# 
+# small_slurm = 
+#   tar_resources(
+#     future = tar_resources_future(
+#       plan = tweak(
+#         batchtools_slurm,
+#         template = glue("/stornext/Bioinf/data/bioinf-data/Papenfuss_lab_projects/people/mangiola.s/third_party_sofware/slurm_batchtools.tmpl"),
+#         resources = list(
+#           ncpus = 2,
+#           memory = 40000,
+#           walltime = 172800
+#         )
+#       )
+#     )
+#   )
+# 
+# big_slurm = 
+#   tar_resources(
+#     future = tar_resources_future(
+#       plan = tweak(
+#         batchtools_slurm,
+#         template = glue("/stornext/Bioinf/data/bioinf-data/Papenfuss_lab_projects/people/mangiola.s/third_party_sofware/slurm_batchtools.tmpl"),
+#         resources = list(
+#           ncpus = 19,
+#           memory = 6000,
+#           walltime = 172800
+#         )
+#       )
+#     )
+#   )
 
 tar_script({
   
@@ -56,29 +58,16 @@ tar_script({
   library(qs)
   
   
-  #-----------------------#
-  # Packages
-  #-----------------------#
-  tar_option_set( 
-    packages = c(
-      "CuratedAtlasQueryR", "stringr", "tibble", "tidySingleCellExperiment", "dplyr", "Matrix",
-      "Seurat", "tidyseurat", "glue", "qs",  "purrr", "tidybulk", "tidySummarizedExperiment", "edgeR"
-    ), 
-    storage = "worker", 
-    retrieval = "worker", 
-    error = "continue", 		
-    format = "qs"
-    #,
-    #debug = "pseudobulk_df_db574b63", # Set the target you want to debug.
-    #cue = tar_cue(mode = "never") # Force skip non-debugging outdated targets.
-  )
+
   
   #-----------------------#
   # Future SLURM
   #-----------------------#
   
   library(future)
-  library("future.batchtools")
+  library(future.callr)
+  plan(callr)
+  # library("future.batchtools")
   # slurm <- 
   # 	`batchtools_slurm` |>
   # 	future::tweak( template = glue("/stornext/Bioinf/data/bioinf-data/Papenfuss_lab_projects/people/mangiola.s/third_party_sofware/slurm_batchtools.tmpl"),
@@ -90,6 +79,27 @@ tar_script({
   # 	)
   # plan(slurm)
   
+  #-----------------------#
+  # Packages
+  #-----------------------#
+  tar_option_set( 
+    packages = c(
+      "CuratedAtlasQueryR", "stringr", "tibble", "tidySingleCellExperiment", "dplyr", "Matrix",
+      "Seurat", "tidyseurat", "glue", "qs",  "purrr", "tidybulk", "tidySummarizedExperiment", "edgeR",
+      "digest"
+    ), 
+    storage = "worker", 
+    retrieval = "worker", 
+    error = "continue", 		
+    format = "qs"
+    #,
+    #debug = "estimates_b84ea256", # Set the target you want to debug.
+    #cue = tar_cue(mode = "never") # Force skip non-debugging outdated targets.
+  )
+  
+  #-----------------------#
+  # Functions
+  #-----------------------#
   se_add_dispersion = function(se_df){
     
     se_df |> 
@@ -126,23 +136,35 @@ tar_script({
         }
       )) |>
       unnest(se) |> 
-      select(-chunk___)
+      select(-chunk___) |>
+      mutate(se_md5 = map_chr(se_chunk, digest))
   }
   
-  analyse = function(se, max_rows_for_matrix_multiplication = NULL, cores = 1, dispersion = NULL){
+  analyse = function(
+    se, max_rows_for_matrix_multiplication = NULL, 
+    cores = 1
+  ){
     
-    dispersion = enquo(dispersion)
-    
-    se |>
+    se |> mutate(se_chunk = map(
+      se_chunk,
+      ~ .x |>
+        
+        # Test
+        test_differential_abundance(
+          ~ dex + (1 | cell),
+          method = "glmmSeq_lme4",
+          cores = cores, 
+          max_rows_for_matrix_multiplication = max_rows_for_matrix_multiplication,
+          .dispersion = dispersion
+        )
       
-      # Test
-      test_differential_abundance(
-        ~ dex + (1 | cell),
-        method = "glmmSeq_lme4",
-        cores = cores, 
-        max_rows_for_matrix_multiplication = max_rows_for_matrix_multiplication,
-        dispersion = !!dispersion
-      )
+    ))
+    
+ 
+    
+     
+    
+    
   }
   
   #-----------------------#
@@ -152,11 +174,11 @@ tar_script({
     
     
     tar_target(file, "temp_data.rds", format = "file"),
-    tar_target(pseudobulk_df_tissue, readRDS(file)),
-    
+    tarchetypes::tar_group_by(pseudobulk_df_tissue, readRDS(file), name),
+  
     # Dispersion
     tar_target(
-      pseudobulk_df_tissue_merged, 
+      pseudobulk_df_tissue_dispersion, 
       pseudobulk_df_tissue |> se_add_dispersion(), 
       pattern = map(pseudobulk_df_tissue)
       #, 
@@ -166,20 +188,24 @@ tar_script({
     # Split in gene chunks
     tar_target(
       pseudobulk_df_tissue_split_by_gene, 
-      pseudobulk_df_tissue_merged |> split_by_gene(), 
-      pattern = map(pseudobulk_df_tissue_merged)
+      pseudobulk_df_tissue_dispersion |> split_by_gene(), 
+      pattern = map(pseudobulk_df_tissue_dispersion)
       #, 
       #resources = small_slurm
+    ),
+    
+    # Parallelise rows
+    tarchetypes::tar_group_by(
+      pseudobulk_df_tissue_split_by_gene_grouped, 
+      pseudobulk_df_tissue_split_by_gene, 
+      name, se_md5
     ),
     
     # Analyse
     tar_target(
       estimates, 
-      pseudobulk_df_tissue_split_by_gene |> mutate(se = map(
-        se,
-        ~ .x |> analyse(max_rows_for_matrix_multiplication = 10000, cores = 18, dispersion)
-      )), 
-      pattern = map(pseudobulk_df_tissue_split_by_gene)
+      pseudobulk_df_tissue_split_by_gene_grouped |> analyse(max_rows_for_matrix_multiplication = 10000, cores = 18) , 
+      pattern = map(pseudobulk_df_tissue_split_by_gene_grouped)
       #, 
       #resources = big_slurm
     )
@@ -189,9 +215,10 @@ tar_script({
 }, ask = FALSE, script = glue("{my_store}.R"))
 
 
-tar_make(
+tar_make_future(
   script = glue("{my_store}.R"),
-  store = my_store
+  store = my_store, 
+  workers = 10
   #, 
   #workers = 200, 
   #garbage_collection = TRUE
