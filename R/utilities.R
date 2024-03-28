c1769588e591ded1167d52017adb688f17e5782f
susansjy22 <susan.sijiayi@gmail.com>
2024-03-28 17:09:13 +1100

    remove seurat disk dependency

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,6 @@
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+
 # Greater than
 gt = function(a, b){	a > b }
 
